892713a0c71df51b1085dff1786b651a7b4f18a2
AnSuHan <glorygem195@gmail.com>
2025-10-22 12:51:47 +0900

    init project
    Flutter 3.35.6 • channel stable • https://github.com/flutter/flutter.git
    Tools • Dart 3.9.2 • DevTools 2.48.0
    --
    안드로이드 관련 버전 수정 필요

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dbHandler
+Flutter 3.35.6 • channel stable • https://github.com/flutter/flutter.git
+Tools • Dart 3.9.2 • DevTools 2.48.0
---
+++ android/app/src/main/kotlin/dbHandler/glorygem/viewEditor/db_handler/MainActivity.kt
@@ -0,0 +1,6 @@
+package dbHandler.glorygem.viewEditor.db_handler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
